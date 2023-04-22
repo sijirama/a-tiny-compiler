27cf30463a8fdeb421d16c17acbb35515333dce3
sijibomi <gbemilesanmi@gmail.com>
2023-04-22 21:01:11 +0100

    chore: configure express sessions

---
+++ src/config/passport.ts
@@ -0,0 +1,6 @@
+import passport from "passport"
+import GoogleStrategy from "passport-google-oauth20"
+
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import path from "path"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import morgan from "morgan"
+import passport from "passport"
+import session from "express-session"
 import { engine } from 'express-handlebars';
 dotenv.config()
 
@@ -12,7 +14,7 @@ import {router as IndexRouter} from "./routes/index"
 //HACK: define constants
 const PORT = process.env.PORT || 3000
 const staticFiles = path.join(__dirname,"public")
-
+const expressSessionSecret = process.env.EXPRESS_SESSION_SECRET
 
 //NOTE: import utilities
 import {connectDB} from "./config/dbConnect"
@@ -22,6 +24,15 @@ const app = Express()
 
 //NOTE: middleware
 app.use(morgan('dev'))
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: String(expressSessionSecret) ,
+  resave: false,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 //NOTE: View engine / handlebars
 app.engine('.hbs', engine({defaultLayout:"main" , extname: '.hbs'}));
@@ -38,7 +49,8 @@ app.use("/" , IndexRouter)
 
 mongoose.connection.once("open" , () => {
     app.listen(PORT , () => {
-       console.clear()
+        console.clear()
+        //console.log(expressSessionSecret)
         console.log("Connected to database")
         console.log(`Server is listening at port ${PORT}`)
     })
