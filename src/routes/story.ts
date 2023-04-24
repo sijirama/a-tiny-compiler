a56e2be8bdfbdafc972d0cbeb3d065788f97375f
sijibomi <gbemilesanmi@gmail.com>
2023-04-24 16:16:47 +0100

    feat: ckeditor configuration

--- src/routes/story.ts
+++ src/routes/story.ts
@@ -1,11 +1,12 @@
 import * as Express from "express"
+import { EnsureGuest } from "../middleware/auth"
 
 const router = Express.Router()
 
 //NOTE: Show Add story page
 //NOTE: ROUTE: GET /story/add
-router.get("/add" , (req, res) => {
-    res.render("add")
+router.get("/add" ,EnsureGuest ,  (req, res) => {
+    res.render("stories/add")
 })
 
 
